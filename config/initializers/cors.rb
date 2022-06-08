db04b14f4259511941e582e81a4421239bd0b682
husseinhesham24 <husseinhesham24@gmail.com>
2022-06-08 17:26:18 +0200

    allow cors for heroku origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,18 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com"
+    origins "http://localhost:3000"
 
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins "https://todolist-mghein.herokuapp.com"
+
+    resource "*",
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
